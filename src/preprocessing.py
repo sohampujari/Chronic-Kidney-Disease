85290cad031cb8e6bbf614ea9c48bad98debde7c
Soham Pujari <149994119+sohampujari@users.noreply.github.com>
2025-03-17 15:06:55 +0530

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,14 +2,15 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder
 
-def preprocess_data(data_path):
-    df = pd.read_csv(data_path)
+def preprocess_data(../Data/kidney_disease.csv):
+    df = pd.read_csv(../Data/kidney_disease.csv)
     
-    # Handling missing values
+df = preprocess_data(../Data/kidney_disease.csv)
+save_data(df, ../Data/kidney_disease.csv)
+
     imputer = SimpleImputer(strategy="mean")
     df.fillna(df.mean(), inplace=True)
 
-    # Encoding categorical features
     label_encoders = {}
     for column in df.select_dtypes(include=['object']).columns:
         label_encoders[column] = LabelEncoder()
